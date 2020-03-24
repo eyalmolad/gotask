d83152674886d833e764ffb4e5b82876a6833d32
eyal molad <eyal.molad@gmail.com>
2020-03-24 20:25:54 +0100

    Add XMLDeserializeCore project.

---
+++ XMLDeserializeCore/Book.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace XMLDeserializeCore
+{
+    public class Book
+    {
+        public string ISBN { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+
+    [XmlRoot("Books", Namespace = "https://gotask.net")]
+    public class Books : List<Book>
+    {
+    }
+}
---
+++ XMLDeserializeCore/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XMLDeserializeCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeserializeSimple();
+        }
+
+        static void DeserializeSimple()
+        {
+            var serializer = new XmlSerializer(typeof(Books));
+
+            // use SimpleBooks.xml or
+            // SimpleBooksMultiNs.xml
+            using (var reader = new FileStream("SimpleBooksMultiNs.xml", FileMode.Open))
+            {
+                var books = (Books)serializer.Deserialize(reader);
+                Console.WriteLine($"Number of books is {books.Count}");
+            }
+        }
+    }
+}
