c5b6cfca89d7ab5717e3dc7e3ad2cc4a1c489fab
eyal molad <eyal.molad@gmail.com>
2020-05-02 14:23:27 +0200

    Add Recent Files project

---
+++ Utils/RecentFiles/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RecentFiles
+{
+   
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            RecentDocsHelpers.ClearAll();
+
+            // add c:\temp\sample.json to recent files.
+            RecentDocsHelpers.AddFile(@"c:\temp\sample.json");
+        }
+    }
+
+    public static class RecentDocsHelpers
+    {
+        public enum ShellAddToRecentDocsFlags
+        {
+            Pidl = 0x001,
+            Path = 0x002,
+            PathW = 0x003
+        }
+
+        [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
+        private static extern void SHAddToRecentDocs(ShellAddToRecentDocsFlags flag, string path);
+
+        public static void AddFile(string path)
+        {
+            SHAddToRecentDocs(ShellAddToRecentDocsFlags.PathW, path);
+        }
+
+        public static void ClearAll()
+        {
+            SHAddToRecentDocs(ShellAddToRecentDocsFlags.Pidl, null);
+        }
+    }
+}
